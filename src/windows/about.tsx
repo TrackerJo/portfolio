b399106d7ad58dfa5a9fc9d81de70c9c9b2c6377
TrackerJo <nkemme54@gmail.com>
2025-07-12 23:45:47 -0400

    added resume

--- src/windows/about.tsx
+++ src/windows/about.tsx
@@ -1,8 +1,79 @@
 import "./about.css";
 
 import ProfilePic from "../assets/profile.png";
+import { useState, useRef, useEffect } from "react";
+
 
 const AboutWindow = ({ handleTerminalButtonClick }: { handleTerminalButtonClick: (e: React.MouseEvent<HTMLDivElement>) => void }) => {
+    const [isVisible, setIsVisible] = useState(false);
+    const [typingText, setTypingText] = useState('');
+
+    const windowRef = useRef<HTMLDivElement>(null);
+    const fullText = "curl -O https://trackerjo.github.io/resume.pdf";
+
+    const [hasFinishedTyping, setHasFinishedTyping] = useState(false);
+
+    useEffect(() => {
+        const observer = new IntersectionObserver(
+            ([entry]) => {
+                if (entry.isIntersecting) {
+                    setIsVisible(true);
+                } else {
+                    setIsVisible(false);
+                }
+            },
+            { threshold: 0.1 }
+        );
+
+        if (windowRef.current) {
+            observer.observe(windowRef.current);
+        }
+
+        return () => observer.disconnect();
+    }, []);
+
+    useEffect(() => {
+        if (hasFinishedTyping) return; // Prevents re-triggering if already finished
+        if (!isVisible) return;
+
+        let i = 0;
+        const timer = setInterval(() => {
+            if (i < fullText.length) {
+                setTypingText(fullText.substring(0, i + 1));
+                i++;
+            } else {
+                clearInterval(timer);
+
+                setHasFinishedTyping(true);
+            }
+        }, 100);
+
+
+
+        return () => clearInterval(timer);
+    }, [isVisible]);
+
+    useEffect(() => {
+        function handleEnterKeyPress(e: KeyboardEvent) {
+            console.log(typingText, fullText, isVisible);
+            console.log(hasFinishedTyping);
+            if (typingText === fullText && isVisible && hasFinishedTyping && e.key === 'Enter') {
+                handleClick();
+            }
+        }
+
+        if (hasFinishedTyping) {
+            document.addEventListener('keydown', handleEnterKeyPress);
+            return () => document.removeEventListener('keydown', handleEnterKeyPress);
+        }
+    }, [typingText, fullText, isVisible, hasFinishedTyping]);
+
+    const handleClick = () => {
+
+        window.open("https://firebasestorage.googleapis.com/v0/b/campusconnect-9.firebasestorage.app/o/Nathaniel%20Kemme%20Nash's%20Resume%202025.pdf?alt=media&token=c06794f6-10f5-4dc9-881b-6b62e06479ff", "_blank");
+
+    }
+
     return (
         <div className="terminal-window" onClick={handleTerminalButtonClick}>
             <div className="terminal-header">
@@ -27,9 +98,10 @@ const AboutWindow = ({ handleTerminalButtonClick }: { handleTerminalButtonClick:
                         </div>
 
                     </div>
+                    <span className="prompt" ref={windowRef}>nathaniel@portfolio:~$</span><span className={`command ${hasFinishedTyping ? "" : "typing-animation"}`}>{typingText}</span> {typingText == fullText ? <button className="project-enter-button" onClick={() => handleClick()} >↵</button> : null}
                 </div>
             </div>
-        </div>
+        </div >
     );
 };
 
